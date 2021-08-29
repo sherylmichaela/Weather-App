5e1df32edd06710847ceec58b32850aa41128414
sherylmichaela <89667702+sherylmichaela@users.noreply.github.com>
2021-08-29 21:40:33 +1000

    Added html, css & js files

---
+++ src/app.js
@@ -0,0 +1,74 @@
+let now = new Date();
+let dateDayTime = document.querySelector(".date-day-time");
+let date = now.getDate();
+let year = now.getFullYear();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+let day = days[now.getDay()];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+
+let time = now.toLocaleString("en-US", {
+  hour: "numeric",
+  minute: "numeric",
+  hour12: true,
+});
+
+dateDayTime.innerHTML = `${date} ${month} ${year}, ${day}, ${time}`;
+
+function displayWeatherCondition(response) {
+  document.querySelector(".city-state").innerHTML = response.data.name;
+  document.querySelector(".current-temp").innerHTML = `${Math.round(
+    response.data.main.temp
+  )}°C`;
+}
+
+function searchCity(city) {
+  let apiKey = "e02aa07e01c4b4ea5bc27a3b29c88d5b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "e02aa07e01c4b4ea5bc27a3b29c88d5b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
