a38ec2817ced4fccac1c5b5fa0c4568ad8059f6d
sherylmichaela <89667702+sherylmichaela@users.noreply.github.com>
2021-10-04 17:05:45 +1100

    Added Googlefont and Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./custom.scss";
 
 function App() {
   return (
@@ -8,6 +9,18 @@ function App() {
     </div>
   );
 }
+
+const FontLink = () => {
+  return (
+    <div className="card">
+      <span className="font-link">
+        This is with Font Link. We are linking the fonts from the Google Fonts.
+      </span>
+    </div>
+  );
+};
+
+export default FontLink;
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
